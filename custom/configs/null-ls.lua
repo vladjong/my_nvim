888a7a03b8afa9151e59bc115364eaaedad245ac
Vlad Gaydenko <vagaydenko@sbercloud.ru>
2023-11-06 11:46:03 +0700

    init my nvchad config

---
+++ custom/configs/null-ls.lua
@@ -0,0 +1,25 @@
+local null_ls = require "null-ls"
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser,
+  b.formatting.golines,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
---
+++ custom/mappings.lua
@@ -0,0 +1,12 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    ["<leader>w"] = { "<cmd>w<CR>", "Save" },
+  },
+}
+
+-- more keybinds!
+
+return M
